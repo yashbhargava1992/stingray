4c1763e55f940ddfdfe7a2964e75c4ca094c1ae1
Daniela Huppenkothen <daniela.huppenkothen@nyu.edu>
2015-10-04 13:26:40 -0400

    Written first test

--- tests/lightcurve_tests.py
+++ tests/lightcurve_tests.py
@@ -1,27 +1,33 @@
 
 import numpy as np
-from ..stingray import lightcurve
+from stingray import lightcurve
 ## TODO: Fix relative imports
 
-class TestLightcurve(object):
+np.random.seed(20150907)
+
+
+class TestLightcurvefromTOA(object):
 
     def setUp(self):
         tstart = 0.0
         tend = 1.0
-        dt = 1.e-8
+        small_dt = 1.e-8
 
-        small_time = np.linspace(tstart, tend, int((tend-tstart)/dt))
+        small_time = np.linspace(tstart, tend, int((tend-tstart)/small_dt))
 
         mean_count_rate = 10000.0
-        mean_counts = mean_count_rate*dt
+        mean_counts = mean_count_rate*small_dt
 
-        self.toa = np.random.poisson(mean_counts, size=small_time.shape[0])
+        poisson_counts = np.random.poisson(mean_counts, size=small_time.shape[0])
+
+        self.toa = small_time[poisson_counts == 1]
+
+        self.dt = 0.1
+        self.lc = lightcurve.Lightcurve(self.toa, dt=self.dt)
 
 
     def test_correct_timeresolution(self):
-        timestep = 0.1
-        lc = lightcurve.Lightcurve(self.toa, timestep=timestep)
-        assert np.isclose(lc.res, timestep)
+        assert np.isclose(self.lc.res, self.dt)
 
 
 
