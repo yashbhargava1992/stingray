38eb25e4bce90d690123f97d703d890108b304e9
Daniela Huppenkothen <daniela.huppenkothen@nyu.edu>
2015-10-17 19:05:27 -0400

    Wrote some light curve making tests

--- tests/test_lightcurve.py
+++ tests/test_lightcurve.py
@@ -6,35 +6,50 @@ np.random.seed(20150907)
 
 
 class TestLightcurve(object):
+
+    def setUp(self):
+        self.times = [1, 2, 3, 4]
+        self.counts = [2, 2, 2, 2]
+        self.dt = 1.0
+
     def test_create(self):
         """
         Demonstrate that we can create a trivial Lightcurve object.
         """
-        times = [1, 2, 3, 4]
-        lc = Lightcurve(times, dt=1.0)
+        lc = Lightcurve(self.times, self.counts)
 
-class TestLightcurvefromTOA(object):
+    def test_lightcurve_from_toa(self):
+        lc = Lightcurve.make_lightcurve(self.times, self.dt)
 
-    def setUp(self):
+    def test_tstart(self):
         tstart = 0.0
-        tend = 1.0
-        small_dt = 1.e-8
+        lc = Lightcurve.make_lightcurve(self.times, self.dt, tstart=0.0)
+        assert lc.tstart == tstart
+        assert lc.time[0] == tstart + 0.5*self.dt
 
-        small_time = np.linspace(tstart, tend, int((tend-tstart)/small_dt))
+    def test_tseg(self):
+        tstart = 0.0
+        tseg = 5.0
+        lc = Lightcurve.make_lightcurve(self.times, self.dt,
+                                        tseg=tseg, tstart=tstart)
 
-        mean_count_rate = 10000.0
-        mean_counts = mean_count_rate*small_dt
+        assert lc.tseg == tseg
+        assert lc.time[-1] - lc.time[0] == tseg-self.dt
 
-        poisson_counts = np.random.poisson(mean_counts, size=small_time.shape[0])
-
-        self.toa = small_time[poisson_counts == 1]
-
-        self.dt = 0.1
-        self.lc = Lightcurve(self.toa, dt=self.dt)
-
+    def test_nondivisble_tseg(self):
+        """
+        If the light curve length input is not divisible by the time resolution,
+        the last (fractional) time bin will be dropped.
+        """
+        tstart = 0.0
+        tseg = 5.5
+        lc = Lightcurve.make_lightcurve(self.times, self.dt,
+                                        tseg=tseg, tstart=tstart)
+        assert lc.tseg == int(tseg/self.dt)
 
     def test_correct_timeresolution(self):
-        assert np.isclose(self.lc.dt, self.dt)
+        lc = Lightcurve.make_lightcurve(self.times, self.dt)
+        assert np.isclose(lc.dt, self.dt)
 
 
-
+    def test_rebinning(self):
