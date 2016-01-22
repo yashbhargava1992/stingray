05a689e227ca644dc334a4809ef725f98add6e62
Daniela Huppenkothen <daniela.huppenkothen@nyu.edu>
2016-01-22 17:02:38 -0500

    Moved classical pvalue function to powerspectrum.py

--- stingray/powerspectrum.py
+++ stingray/powerspectrum.py
@@ -8,7 +8,93 @@ import scipy.optimize
 
 import stingray.lightcurve as lightcurve
 import stingray.utils as utils
-from stingray.classical_significances import classical_pvalue
+
+def classical_pvalue(power, nspec):
+    """
+    Compute the probability of detecting the current power under
+    the assumption that there is no periodic oscillation in the data.
+
+    This computes the single-trial p-value that the power was
+    observed under the null hypothesis that there is no signal in
+    the data.
+
+    Important: the underlying assumptions that make this calculation valid
+    are:
+    (1) the powers in the power spectrum follow a chi-square distribution
+    (2) the power spectrum is normalized according to Leahy (1984), such
+    that the powers have a mean of 2 and a variance of 4
+    (3) there is only white noise in the light curve. That is, there is no
+    aperiodic variability that would change the overall shape of the power
+    spectrum.
+
+    Also note that the p-value is for a *single trial*, i.e. the power currently
+    being tested. If more than one power or more than one power spectrum are
+    being tested, the resulting p-value must be corrected for the number
+    of trials (Bonferroni correction).
+
+    Mathematical formulation in Groth, 1975.
+    Original implementation in IDL by Anna L. Watts.
+
+    Parameters
+    ----------
+    power :  float
+        The squared Fourier amplitude of a spectrum to be evaluated
+
+    nspec : int
+        The number of spectra or frequency bins averaged in `power`.
+        This matters because averaging spectra or frequency bins increases
+        the signal-to-noise ratio, i.e. makes the statistical distributions
+        of the noise narrower, such that a smaller power might be very
+        significant in averaged spectra even though it would not be in a single
+        power spectrum.
+
+    """
+
+    assert np.isfinite(power), "power must be a finite floating point number!"
+    assert power > 0.0, "power must be a positive real number!"
+    assert np.isfinite(nspec), "nspec must be a finite integer number"
+    assert nspec >= 1, "nspec must be larger or equal to 1"
+    assert np.isclose(nspec%1, 0), "nspec must be an integer number!"
+
+    ## If the power is really big, it's safe to say it's significant,
+    ## and the p-value will be nearly zero
+    if power*nspec > 30000:
+        print("Probability of no signal too miniscule to calculate.")
+        return 0.0
+
+    else:
+        pval = _pavnosigfun(power, nspec)
+        return pval
+
+
+
+def _pavnosigfun(power, nspec):
+    """
+    Helper function doing the actual calculation of the p-value.
+    """
+    sum = 0.0
+    m = nspec-1
+
+    pn = power*nspec
+
+    while m >= 0:
+
+        s = 0.0
+        for i in xrange(int(m)-1):
+            s += np.log(float(m-i))
+
+        logterm = m*np.log(pn/2.0) - pn/2.0 - s
+        term = np.exp(logterm)
+        ratio = sum/term
+
+        if ratio > 1.0e15:
+            return sum
+
+        sum += term
+        m -= 1
+
+    return sum
+
 
 
 class Powerspectrum(object):
--- tests/test_powerspectrum.py
+++ tests/test_powerspectrum.py
@@ -4,9 +4,10 @@ import numpy as np
 
 from nose.tools import raises
 
-
 from stingray import Lightcurve
 from stingray import Powerspectrum, AveragedPowerspectrum
+
+from stingray.powerspectrum import classical_pvalue
 
 np.random.seed(20150907)
 
@@ -360,3 +361,102 @@ class TestAveragedPowerspectrum(object):
         segment_size = 0.5
 
         assert AveragedPowerspectrum(lc_all, segment_size)
+
+
+class TestClassicalSignificances(object):
+
+    def test_function_runs(self):
+        power = 2.0
+        nspec = 1.0
+        classical_pvalue(power, nspec)
+
+    @raises(AssertionError)
+    def test_power_is_not_infinite(self):
+        power = np.inf
+        nspec = 1
+        classical_pvalue(power, nspec)
+
+    @raises(AssertionError)
+    def test_power_is_not_infinite2(self):
+        power = -np.inf
+        nspec = 1
+        classical_pvalue(power, nspec)
+
+    @raises(AssertionError)
+    def test_power_is_non_nan(self):
+        power = np.nan
+        nspec = 1
+        classical_pvalue(power, nspec)
+
+    @raises(AssertionError)
+    def test_power_is_positive(self):
+        power = -2.0
+        nspec = 1.0
+        classical_pvalue(power, nspec)
+
+    @raises(AssertionError)
+    def test_nspec_is_not_infinite(self):
+        power = 2.0
+        nspec = np.inf
+        classical_pvalue(power, nspec)
+
+    @raises(AssertionError)
+    def test_nspec_is_not_infinite2(self):
+        power = 2.0
+        nspec = -np.inf
+        classical_pvalue(power, nspec)
+
+    @raises(AssertionError)
+    def test_nspec_is_not_nan(self):
+        power = 2.0
+        nspec = np.nan
+        classical_pvalue(power, nspec)
+
+    @raises(AssertionError)
+    def test_nspec_is_positive(self):
+        power = 2.0
+        nspec = -1.0
+        classical_pvalue(power, nspec)
+
+    @raises(AssertionError)
+    def test_nspec_is_nonzero(self):
+        power = 2.0
+        nspec = 0.0
+        classical_pvalue(power, nspec)
+
+    @raises(AssertionError)
+    def test_nspec_is_an_integer_number(self):
+        power = 2.0
+        nspec = 2.5
+        classical_pvalue(power, nspec)
+
+    def test_nspec_float_type_okay(self):
+        power = 2.0
+        nspec = 2.0
+        classical_pvalue(power, nspec)
+
+    def test_pvalue_decreases_with_increasing_power(self):
+        power1 = 2.0
+        power2 = 20.0
+        nspec = 1.0
+        pval1 = classical_pvalue(power1, nspec)
+        pval2 = classical_pvalue(power2, nspec)
+
+        assert pval1-pval2 > 0.0
+
+    def test_pvalue_must_decrease_with_increasing_nspec(self):
+
+        power = 3.0
+        nspec1 = 1.0
+        nspec2 = 10.0
+
+        pval1 = classical_pvalue(power, nspec1)
+        pval2 = classical_pvalue(power, nspec2)
+
+        assert pval1-pval2 > 0.0
+
+    def test_very_large_powers_produce_zero_prob(self):
+        power = 31000.0
+        nspec = 1
+        pval = classical_pvalue(power, nspec)
+        assert np.isclose(pval, 0.0)
