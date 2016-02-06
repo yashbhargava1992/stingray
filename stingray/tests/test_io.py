5feb056b31fb01067d9c155812e4d1412c0df7cd
mbachett <matteo.bachetti@irap.omp.eu>
2016-02-06 18:09:52 +0100

    Tests for io moved to stingray/tests directory, following the astropy model, and using relative imports

--- stingray/tests/test_io.py
+++ stingray/tests/test_io.py
@@ -2,7 +2,6 @@ from __future__ import (absolute_import, unicode_literals, division,
                         print_function)
 import numpy as np
 import unittest
-import stingray
 import os
 curdir = os.path.abspath(os.path.dirname(__file__))
 datadir = os.path.join(curdir, 'data')
@@ -14,38 +13,42 @@ class TestAll(unittest.TestCase):
 
     def test_crossgti1(self):
         """Test the basic working of the intersection of GTIs."""
+        from ..io import cross_gtis
         gti1 = np.array([[1, 4]])
         gti2 = np.array([[2, 5]])
-        newgti = stingray.io.cross_gtis([gti1, gti2])
+        newgti = cross_gtis([gti1, gti2])
 
         assert np.all(newgti == [[2, 4]]), 'GTIs do not coincide!'
 
     def test_crossgti2(self):
         """A more complicated example of intersection of GTIs."""
+        from ..io import cross_gtis
         gti1 = np.array([[1, 2], [4, 5], [7, 10], [11, 11.2], [12.2, 13.2]])
         gti2 = np.array([[2, 5], [6, 9], [11.4, 14]])
-        newgti = stingray.io.cross_gtis([gti1, gti2])
+        newgti = cross_gtis([gti1, gti2])
 
         assert np.all(newgti == [[4.0, 5.0], [7.0, 9.0], [12.2, 13.2]]), \
             'GTIs do not coincide!'
 
     def test_bti(self):
         """Test the inversion of GTIs."""
+        from ..io import get_btis
         gti = np.array([[1, 2], [4, 5], [7, 10], [11, 11.2], [12.2, 13.2]])
-        bti = stingray.io.get_btis(gti)
+        bti = get_btis(gti)
 
         assert np.all(bti == [[2, 4], [5, 7], [10, 11], [11.2, 12.2]]), \
             'BTI is wrong!, %s' % repr(bti)
 
     def test_common_name(self):
         """Test the common_name function."""
-        a = 'A_3-50_A.nc'
-        b = 'B_3-50_B.nc'
-        assert stingray.io.common_name(a, b) == '3-50'
+        from ..io import common_name
+        a = 'A_3-50_A'
+        b = 'B_3-50_B'
+        assert common_name(a, b) == '3-50'
 
     def test_high_precision_keyword(self):
         """Test high precision FITS keyword read."""
-        from stingray.io import high_precision_keyword_read
+        from ..io import high_precision_keyword_read
         hdr = {"MJDTESTI": 100, "MJDTESTF": np.longdouble(0.5),
                "CIAO": np.longdouble(0.)}
         assert \
@@ -58,5 +61,6 @@ class TestAll(unittest.TestCase):
 
     def test_event_file_read(self):
         """Test event file reading."""
+        from ..io import load_events_and_gtis
         fname = os.path.join(datadir, 'monol_testA.evt')
-        stingray.io.load_events_and_gtis(fname, additional_columns=["PI"])
+        load_events_and_gtis(fname, additional_columns=["PI"])
