c896d0d914d4b43a2b2e867d15ad0db080c99d0c
Usman Mahmood <usmanwardag@gmail.com>
2016-05-15 11:47:07 +0500

    Removes astropy_init

--- stingray/simulator/__init__.py
+++ stingray/simulator/__init__.py
@@ -7,7 +7,7 @@ Library of Time Series Methods For Astronomical X-ray Data.
 # Affiliated packages may add whatever they like to this file, but
 # should keep this content at the top.
 # ----------------------------------------------------------------------------
-from ._astropy_init import *
+from .._astropy_init import *
 # ----------------------------------------------------------------------------
 
 # For egg_info test builds to pass, put package imports here.
