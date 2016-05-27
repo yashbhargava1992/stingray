3b1607bec590577d594b2281627e15be07fc2518
Himanshu Mishra <himanshu2014iit@gmail.com>
2016-05-27 17:34:18 +0530

    Remove underscore from lc_1 and nphots_1

--- stingray/crossspectrum.py
+++ stingray/crossspectrum.py
@@ -12,7 +12,7 @@ import stingray.utils as utils
 
 class Crossspectrum(object):
 
-    def __init__(self, lc_1=None, lc_2=None, norm='none'):
+    def __init__(self, lc1=None, lc2=None, norm='none'):
         """
         Make a cross spectrum from a (binned) light curve.
         You can also make an empty Crossspectrum object to populate with your
@@ -21,10 +21,10 @@ class Crossspectrum(object):
 
         Parameters
         ----------
-        lc_1: lightcurve.Lightcurve object, optional, default None
+        lc1: lightcurve.Lightcurve object, optional, default None
             The first light curve data for the channel/band of interest.
-            
-        lc_2: lightcurve.Lightcurve object, optional, default None
+
+        lc2: lightcurve.Lightcurve object, optional, default None
             The light curve data for the reference band.
 
         norm: {'frac', 'abs', 'leahy', 'none'}, default 'none'
@@ -48,10 +48,10 @@ class Crossspectrum(object):
             The number of data points/time bins in one segment of the light 
             curves.
 
-        nphots_1: float
+        nphots1: float
             The total number of photons in light curve 1
             
-        nphots_2: float
+        nphots2: float
             The total number of photons in light curve 2
         """
 
@@ -63,9 +63,9 @@ class Crossspectrum(object):
         self.norm = norm.lower()
 
         ## check if input data is a Lightcurve object, if not make one or
-        ## make an empty Crossspectrum object if lc_1 == None or lc_2 == None
-        if lc_1 is None or lc_2 is None:
-            if lc_1 is not None or lc_2 is not None:
+        ## make an empty Crossspectrum object if lc1 == None or lc2 == None
+        if lc1 is None or lc2 is None:
+            if lc1 is not None or lc2 is not None:
                  print("You can't do a cross spectrum with just one "
                          "light curve!")
             # else:
@@ -73,62 +73,62 @@ class Crossspectrum(object):
             self.freq = None
             self.cs = None
             self.df = None
-            self.nphots_1 = None
-            self.nphots_2 = None
+            self.nphots1 = None
+            self.nphots2 = None
             self.m = 1
             self.n = None
             return
-        self._make_crossspectrum(lc_1, lc_2)
+        self._make_crossspectrum(lc1, lc2)
 
-    def _make_crossspectrum(self, lc_1, lc_2):
+    def _make_crossspectrum(self, lc1, lc2):
 
         ## make sure the inputs work!
-        assert isinstance(lc_1, lightcurve.Lightcurve), \
-                        "lc_1 must be a lightcurve.Lightcurve object!"
-        assert isinstance(lc_2, lightcurve.Lightcurve), \
-                        "lc_2 must be a lightcurve.Lightcurve object!"
+        assert isinstance(lc1, lightcurve.Lightcurve), \
+                        "lc1 must be a lightcurve.Lightcurve object!"
+        assert isinstance(lc2, lightcurve.Lightcurve), \
+                        "lc2 must be a lightcurve.Lightcurve object!"
 
 
         ## total number of photons is the sum of the
         ## counts in the light curve
-        self.nphots_1 = np.sum(lc_1.counts)
-        self.nphots_2 = np.sum(lc_2.counts)
+        self.nphots1 = np.sum(lc1.counts)
+        self.nphots2 = np.sum(lc2.counts)
 
 
         ## the number of data points in the light curve
-        assert lc_1.counts.shape[0] == lc_2.counts.shape[0], \
+        assert lc1.counts.shape[0] == lc2.counts.shape[0], \
             "Light curves do not have same number of time bins per segment."
-        self.n = lc_1.counts.shape[0]
+        self.n = lc1.counts.shape[0]
 
-        assert lc_1.tseg == lc_2.tseg, "Light curves do not have same tseg."
+        assert lc1.tseg == lc2.tseg, "Light curves do not have same tseg."
 
         ## the frequency resolution
-        self.df = 1.0/lc_1.tseg
+        self.df = 1.0/lc1.tseg
 
         ## the number of averaged periodograms in the final output
         ## This should *always* be 1 here
         self.m = 1
 
         ## make the actual Fourier transform and compute cross spectrum
-        self.freq, self.unnorm_cross = self._fourier_cross(lc_1, lc_2)
+        self.freq, self.unnorm_cross = self._fourier_cross(lc1, lc2)
 
         ## If co-spectrum is desired, normalize here. Otherwise, get raw back
         ## with the imaginary part still intact.
-        self.cs = self._normalize_crossspectrum(self.unnorm_cross, lc_1.tseg)
+        self.cs = self._normalize_crossspectrum(self.unnorm_cross, lc1.tseg)
 
-    def _fourier_cross(self, lc_1, lc_2):
+    def _fourier_cross(self, lc1, lc2):
         """
         Fourier transform the two light curves, then compute the cross spectrum.
-        computed as CS = lc_1 x lc_2* (where lc_2 is the one that gets
+        computed as CS = lc1 x lc2* (where lc2 is the one that gets
         complex-conjugated)
 
         Parameters
         ----------
-        lc_1: lightcurve.Lightcurve object
+        lc1: lightcurve.Lightcurve object
             One light curve to be Fourier transformed. Ths is the band of
             interest or channel of interest.
 
-        lc_2: lightcurve.Lightcurve object
+        lc2: lightcurve.Lightcurve object
             Another light curve to be Fourier transformed. This is the reference
             band.
 
@@ -138,14 +138,14 @@ class Crossspectrum(object):
             The squared absolute value of the Fourier amplitudes
 
         """
-        fourier_1 = scipy.fftpack.fft(lc_1.counts)  # do Fourier transform 1
-        fourier_2 = scipy.fftpack.fft(lc_2.counts)  # do Fourier transform 2
+        fourier_1 = scipy.fftpack.fft(lc1.counts)  # do Fourier transform 1
+        fourier_2 = scipy.fftpack.fft(lc2.counts)  # do Fourier transform 2
 
-        assert lc_1.counts.shape[0] == lc_2.counts.shape[0], \
+        assert lc1.counts.shape[0] == lc2.counts.shape[0], \
                 "Light curves do not have the same shape."
-        assert lc_1.dt == lc_2.dt, \
+        assert lc1.dt == lc2.dt, \
                 "Light curves do not have same time binning dt."
-        freqs = scipy.fftpack.fftfreq(lc_1.counts.shape[0], lc_1.dt)
+        freqs = scipy.fftpack.fftfreq(lc1.counts.shape[0], lc1.dt)
         cross = fourier_1[freqs > 0] * np.conj(fourier_2[freqs > 0])
         return freqs[freqs > 0], cross
 
@@ -179,8 +179,8 @@ class Crossspectrum(object):
         bin_cs.df = df
         bin_cs.n = self.n
         bin_cs.norm = self.norm
-        bin_cs.nphots_1 = self.nphots_1
-        bin_cs.nphots_2 = self.nphots_2
+        bin_cs.nphots1 = self.nphots1
+        bin_cs.nphots2 = self.nphots2
         bin_cs.m = int(step_size)
 
         return bin_cs
@@ -207,7 +207,7 @@ class Crossspectrum(object):
 
         ## The "effective" count rate is the geometrical mean of the count rates
         ## of the two light curves; need to divide by tseg to have count rate
-        actual_mean = np.sqrt(self.nphots_1 * self.nphots_2 / tseg)
+        actual_mean = np.sqrt(self.nphots1 * self.nphots2 / tseg)
 
         assert actual_mean > 0.0, \
                 "Mean count rate is <= 0. Something went wrong."
@@ -235,7 +235,7 @@ class Crossspectrum(object):
 
 class AveragedCrossspectrum(Crossspectrum):
 
-    def __init__(self, lc_1, lc_2, segment_size=1, norm='none'):
+    def __init__(self, lc1, lc2, segment_size=1, norm='none'):
         """
         Make an averaged cross spectrum from a light curve by segmenting two 
         light curves, Fourier-transforming each segment and then averaging the
@@ -243,19 +243,19 @@ class AveragedCrossspectrum(Crossspectrum):
         
         Parameters
         ----------
-        lc_1: lightcurve.Lightcurve object OR
+        lc1: lightcurve.Lightcurve object OR
             iterable of lightcurve.Lightcurve objects
             One light curve data to be Fourier-transformed. This is the band
             of interest or channel of interest.
 
-        lc_2: lightcurve.Lightcurve object OR
+        lc2: lightcurve.Lightcurve object OR
             iterable of lightcurve.Lightcurve objects
             Second light curve data to be Fourier-transformed. This is the 
             reference band.
             
         segment_size: float, default 1 second
             The size of each segment to average. Note that if the total duration
-            of each Lightcurve object in lc_1 or lc_2 is not an integer multiple
+            of each Lightcurve object in lc1 or lc2 is not an integer multiple
             of the segment_size, then any fraction left-over at the end of the
             time series will be lost. Otherwise you introduce artefacts.
 
@@ -279,10 +279,10 @@ class AveragedCrossspectrum(Crossspectrum):
         n: int
             The number of time bins per segment of light curve?
             
-        nphots_1: float
+        nphots1: float
             The total number of photons in the first (interest) light curve
         
-        nphots_2: float
+        nphots2: float
             The total number of photons in the second (reference) light curve
 
         """
@@ -297,72 +297,72 @@ class AveragedCrossspectrum(Crossspectrum):
 
         self.segment_size = segment_size
 
-        Crossspectrum.__init__(self, lc_1, lc_2, self.norm)
+        Crossspectrum.__init__(self, lc1, lc2, self.norm)
 
         return
 
-    def _make_segment_csd(self, lc_1, lc_2, segment_size):
+    def _make_segment_csd(self, lc1, lc2, segment_size):
 
         ## TODO: need to update this for making cross spectra.
-        assert isinstance(lc_1, lightcurve.Lightcurve)
-        assert isinstance(lc_2, lightcurve.Lightcurve)
+        assert isinstance(lc1, lightcurve.Lightcurve)
+        assert isinstance(lc2, lightcurve.Lightcurve)
 
-        assert lc_1.dt == lc_2.dt, \
+        assert lc1.dt == lc2.dt, \
             "Light curves do not have same time binning dt."
-        assert lc_1.tseg == lc_2.tseg, "Lightcurves do not have same tseg."
+        assert lc1.tseg == lc2.tseg, "Lightcurves do not have same tseg."
 
         ## number of bins per segment
-        nbins = int(segment_size/lc_1.dt)
+        nbins = int(segment_size/lc1.dt)
         start_ind = 0
         end_ind = nbins
 
         cs_all = []
-        nphots_1_all = []
-        nphots_2_all = []
+        nphots1_all = []
+        nphots2_all = []
 
-        while end_ind <= lc_1.counts.shape[0]:
-            time_1 = lc_1.time[start_ind:end_ind]
-            counts_1 = lc_1.counts[start_ind:end_ind]
-            time_2 = lc_2.time[start_ind:end_ind]
-            counts_2 = lc_2.counts[start_ind:end_ind]
-            lc_1_seg = lightcurve.Lightcurve(time_1, counts_1)
-            lc_2_seg = lightcurve.Lightcurve(time_2, counts_2)
-            cs_seg = Crossspectrum(lc_1_seg, lc_2_seg, norm=self.norm)
+        while end_ind <= lc1.counts.shape[0]:
+            time_1 = lc1.time[start_ind:end_ind]
+            counts_1 = lc1.counts[start_ind:end_ind]
+            time_2 = lc2.time[start_ind:end_ind]
+            counts_2 = lc2.counts[start_ind:end_ind]
+            lc1_seg = lightcurve.Lightcurve(time_1, counts_1)
+            lc2_seg = lightcurve.Lightcurve(time_2, counts_2)
+            cs_seg = Crossspectrum(lc1_seg, lc2_seg, norm=self.norm)
             cs_all.append(cs_seg)
-            nphots_1_all.append(np.sum(lc_1_seg.counts))
-            nphots_2_all.append(np.sum(lc_2_seg.counts))
+            nphots1_all.append(np.sum(lc1_seg.counts))
+            nphots2_all.append(np.sum(lc2_seg.counts))
             start_ind += nbins
             end_ind += nbins
 
-        return cs_all, nphots_1_all, nphots_2_all
+        return cs_all, nphots1_all, nphots2_all
 
-    def _make_crossspectrum(self, lc_1, lc_2):
+    def _make_crossspectrum(self, lc1, lc2):
 
         ## chop light curves into segments
-        if isinstance(lc_1, lightcurve.Lightcurve) and \
-                isinstance(lc_2, lightcurve.Lightcurve):
-            cs_all, nphots_1_all, nphots_2_all = self._make_segment_csd(lc_1,
-                                                        lc_2, self.segment_size)
+        if isinstance(lc1, lightcurve.Lightcurve) and \
+                isinstance(lc2, lightcurve.Lightcurve):
+            cs_all, nphots1_all, nphots2_all = self._make_segment_csd(lc1,
+                                                        lc2, self.segment_size)
         else:
-            cs_all, nphots_1_all, nphots_2_all = [], [], []
-            ## TODO: should be using izip from iterables if lc_1 or lc_2 could
+            cs_all, nphots1_all, nphots2_all = [], [], []
+            ## TODO: should be using izip from iterables if lc1 or lc2 could
             ## be long
-            for lc_1_seg, lc_2_seg in zip(lc_1, lc_2):
-                cs_sep, nphots_1_sep, nphots_2_sep = self._make_segment_csd(lc_1_seg, lc_2_seg,
+            for lc1_seg, lc2_seg in zip(lc1, lc2):
+                cs_sep, nphots1_sep, nphots2_sep = self._make_segment_csd(lc1_seg, lc2_seg,
                                                             self.segment_size)
 
                 cs_all.append(cs_sep)
-                nphots_1_all.append(nphots_1_sep)
-                nphots_2_all.append(nphots_2_sep)
+                nphots1_all.append(nphots1_sep)
+                nphots2_all.append(nphots2_sep)
 
             cs_all = np.hstack(cs_all)
-            nphots_1_all = np.hstack(nphots_1_all)
-            nphots_2_all = np.hstack(nphots_2_all)
+            nphots1_all = np.hstack(nphots1_all)
+            nphots2_all = np.hstack(nphots2_all)
 
 
         m = len(cs_all)
-        nphots_1 = np.mean(nphots_1_all)
-        nphots_2 = np.mean(nphots_2_all)
+        nphots1 = np.mean(nphots1_all)
+        nphots2 = np.mean(nphots2_all)
 
         cs_avg = np.zeros_like(cs_all[0].cs)
         for cs in cs_all:
@@ -375,6 +375,6 @@ class AveragedCrossspectrum(Crossspectrum):
         self.m = m
         self.df = cs_all[0].df
         self.n = cs_all[0].n
-        self.nphots_1 = nphots_1
-        self.nphots_2 = nphots_2
+        self.nphots1 = nphots1
+        self.nphots2 = nphots2
 
