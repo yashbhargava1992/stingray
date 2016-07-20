6daee09dde93a05662b557da1333a956f5804687
Daniela Huppenkothen <daniela.huppenkothen@nyu.edu>
2016-07-20 12:32:38 -0400

    Added __init__ file to the submodule

---
+++ stingray/modeling/__init__.py
@@ -0,0 +1,17 @@
+# Licensed under MIT license - see LICENSE.rst
+
+"""
+Library of Time Series Methods For Astronomical X-ray Data.
+"""
+
+# Affiliated packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+from .._astropy_init import *
+# ----------------------------------------------------------------------------
+
+# For egg_info test builds to pass, put package imports here.
+if not _ASTROPY_SETUP_:
+    from stingray.lightcurve import *
+    from stingray.utils import *
+    from stingray.powerspectrum import *
