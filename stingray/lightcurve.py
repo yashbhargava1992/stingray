9bf9b237ae313c6c94b2f7a4702ba0607b696db3
mbachett <matteo.bachetti@irap.omp.eu>
2016-07-29 16:53:09 +0200

    Add GTI management for light curves

--- stingray/lightcurve.py
+++ stingray/lightcurve.py
@@ -8,14 +8,15 @@ import logging
 import numpy as np
 import stingray.io as io
 import stingray.utils as utils
-from stingray.utils import simon
+from stingray.utils import simon, assign_value_if_none
+from stingray.gti import cross_two_gtis, join_gtis
 
 
 __all__ = ["Lightcurve"]
 
 
 class Lightcurve(object):
-    def __init__(self, time, counts, input_counts=True):
+    def __init__(self, time, counts, input_counts=True, gti=None):
         """
         Make a light curve object from an array of time stamps and an
         array of counts.
@@ -34,6 +35,9 @@ class Lightcurve(object):
             If True, the code assumes that the input data in 'counts'
             is in units of counts/bin. If False, it assumes the data
             in 'counts' is in counts/second.
+
+        gti: [[start0, end0], [start1, end1], ...]
+            Good Time Intervals
 
         Attributes
         ----------
@@ -57,6 +61,9 @@ class Lightcurve(object):
 
         tstart: float
             The start time of the light curve.
+
+        gti: [[start0, end0], [start1, end1], ...]
+            Good Time Intervals
 
         """
 
@@ -94,6 +101,10 @@ class Lightcurve(object):
 
         self.tseg = self.time[-1] - self.time[0] + self.dt
         self.tstart = self.time[0] - 0.5*self.dt
+        self.gti = \
+            np.asarray(assign_value_if_none(gti,
+                                            [[self.tstart,
+                                              self.tstart + self.tseg]]))
 
     def __add__(self, other):
         """
@@ -102,13 +113,17 @@ class Lightcurve(object):
         This magic method adds two Lightcurve objects having the same time
         array such that the corresponding counts arrays get summed up.
 
+        GTIs are crossed, so that only common intervals are saved.
+
         Example
         -------
         >>> time = [5, 10, 15]
         >>> count1 = [300, 100, 400]
         >>> count2 = [600, 1200, 800]
-        >>> lc1 = Lightcurve(time, count1)
-        >>> lc2 = Lightcurve(time, count2)
+        >>> gti1 = [[0, 20]]
+        >>> gti2 = [[0, 25]]
+        >>> lc1 = Lightcurve(time, count1, gti=gti1)
+        >>> lc2 = Lightcurve(time, count2, gti=gti2)
         >>> lc = lc1 + lc2
         >>> lc.counts
         array([ 900, 1300, 1200])
@@ -123,8 +138,9 @@ class Lightcurve(object):
                                  "of same dimension and equal.")
 
         new_counts = np.add(self.counts, other.counts)
+        common_gti = cross_two_gtis(self.gti, other.gti)
 
-        lc_new = Lightcurve(self.time, new_counts)
+        lc_new = Lightcurve(self.time, new_counts, gti=common_gti)
 
         return lc_new
 
@@ -136,13 +152,17 @@ class Lightcurve(object):
         time array such that the corresponding counts arrays interferes with
         each other.
 
+        GTIs are crossed, so that only common intervals are saved.
+
         Example
         -------
         >>> time = [10, 20, 30]
         >>> count1 = [600, 1200, 800]
         >>> count2 = [300, 100, 400]
-        >>> lc1 = Lightcurve(time, count1)
-        >>> lc2 = Lightcurve(time, count2)
+        >>> gti1 = [[0, 20]]
+        >>> gti2 = [[0, 25]]
+        >>> lc1 = Lightcurve(time, count1, gti=gti1)
+        >>> lc2 = Lightcurve(time, count2, gti=gti2)
         >>> lc = lc1 - lc2
         >>> lc.counts
         array([ 300, 1100,  400])
@@ -157,8 +177,9 @@ class Lightcurve(object):
                                  "of same dimension and equal.")
 
         new_counts = np.subtract(self.counts, other.counts)
+        common_gti = cross_two_gtis(self.gti, other.gti)
 
-        lc_new = Lightcurve(self.time, new_counts)
+        lc_new = Lightcurve(self.time, new_counts, gti=common_gti)
 
         return lc_new
 
@@ -180,7 +201,7 @@ class Lightcurve(object):
         >>> lc_new.counts
         array([100, 100, 100])
         """
-        lc_new = Lightcurve(self.time, -1*self.counts)
+        lc_new = Lightcurve(self.time, -1*self.counts, gti=self.gti)
 
         return lc_new
 
@@ -236,7 +257,7 @@ class Lightcurve(object):
                              "object !")
 
     @staticmethod
-    def make_lightcurve(toa, dt, tseg=None, tstart=None):
+    def make_lightcurve(toa, dt, tseg=None, tstart=None, gti=None):
 
         """
         Make a light curve out of photon arrival times.
@@ -263,6 +284,9 @@ class Lightcurve(object):
             The start time of the light curve.
             If this is None, the arrival time of the first photon will be used
             as the start time of the light curve.
+
+        gti: [[start0, end0], [start1, end1], ...]
+            Good Time Intervals
 
         Returns
         -------
@@ -299,7 +323,7 @@ class Lightcurve(object):
 
         counts = np.asarray(counts)
 
-        return Lightcurve(time, counts)
+        return Lightcurve(time, counts, gti=gti)
 
     def rebin_lightcurve(self, dt_new, method='sum'):
         """
@@ -342,6 +366,8 @@ class Lightcurve(object):
         objects. The count per bin in the resulting object will be the
         individual count per bin, or the average in case of overlapping
         time arrays of both lightcurve objects.
+
+        Good Time intervals are also joined.
 
         Note : Time array of both lightcurves should not overlap each other.
 
@@ -409,7 +435,9 @@ class Lightcurve(object):
 
         new_counts = np.asarray(new_counts)
 
-        lc_new = Lightcurve(new_time, new_counts)
+        gti = join_gtis(self.gti, other.gti)
+
+        lc_new = Lightcurve(new_time, new_counts, gti=gti)
 
         return lc_new
 
@@ -473,8 +501,12 @@ class Lightcurve(object):
         """Private method for truncation using index values."""
         time_new = self.time[start:stop]
         counts_new = self.counts[start:stop]
+        gti = \
+            cross_two_gtis(self.gti,
+                           np.asarray([[self.time[start] - 0.5 * self.dt,
+                                        time_new[-1] + 0.5 * self.dt]]) )
 
-        return Lightcurve(time_new, counts_new)
+        return Lightcurve(time_new, counts_new, gti=gti)
 
     def _truncate_by_time(self, start, stop):
         """Private method for truncation using time values."""
