Remove wrong error calculation method from AveragedCovariancespectrum

stingray/covariancespectrum.py
@@ -226,7 +226,7 @@ class Covariancespectrum(object):
             self.covar_error = {}
 
     def _construct_energy_covar(self, energy_events, energy_covar,
-                                covar_error=None, xs_var=None):
+                                xs_var=None):
         """Form the actual output covaraince dictionary and array."""
         self._init_energy_covar(energy_events, energy_covar)
 
@@ -260,9 +260,7 @@ class Covariancespectrum(object):
             if not self.avg_covar:
                 self.covar_error[energy] = self._calculate_covariance_error(
                                                 lc, lc_ref)
-            else:
-                covar_error[energy] = self._calculate_covariance_error(
-                                           lc, lc_ref)
+
             # Excess variance in ref band
             xs_var[energy] = self._calculate_excess_variance(lc_ref)
 
@@ -436,27 +434,22 @@ class AveragedCovariancespectrum(Covariancespectrum):
 
             energy_covar = {}
             xs_var = {}
-            covar_error = {}
             self._construct_energy_covar(energy_events, energy_covar,
-                                         covar_error, xs_var)
+                                         xs_var)
 
             if n == 0:  # Declare
                 self.energy_covar = energy_covar
                 self.xs_var = xs_var
-                self.covar_error = covar_error
             else:  # Sum up
                 for key in energy_covar.keys():
                     self.energy_covar[key] = self.energy_covar.get(key, 0) + \
                                              energy_covar[key]
                     self.xs_var[key] = self.xs_var.get(key, 0) + xs_var[key]
-                    self.covar_error[key] = self.covar_error.get(key, 0) + \
-                                            covar_error[key]
 
         # Now divide with total number of bins for averaging
         for key in self.energy_covar.keys():
             self.energy_covar[key] /= self.nbins
             self.xs_var[key] /= self.nbins
-            self.covar_error[key] /= self.nbins
 
         self.unnorm_covar = np.vstack(self.energy_covar.items())
 
@@ -464,4 +457,3 @@ class AveragedCovariancespectrum(Covariancespectrum):
             self.energy_covar[key] = value / (self.xs_var[key])**0.5
 
         self.covar = np.vstack(self.energy_covar.items())
-        self.covar_error = np.vstack(self.covar_error.items())