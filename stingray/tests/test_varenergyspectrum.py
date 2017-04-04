76a1f7e3e07c181e8a90c5cdf1b1890aaa6c31ca
Matteo Bachetti <matteo@matteobachetti.it>
2017-04-04 17:06:43 +0200

    Machinery to create base and reference light curves

--- stingray/tests/test_varenergyspectrum.py
+++ stingray/tests/test_varenergyspectrum.py
@@ -10,7 +10,7 @@ class TestPowerspectrum(object):
     @classmethod
     def setup_class(cls):
         tstart = 0.0
-        tend = 1.0
+        tend = 10.0
         nphot = 100
         cls.events = EventList(np.random.uniform(tstart, tend, nphot),
                                 pha=np.random.uniform(0.3, 12, nphot))
@@ -18,11 +18,15 @@ class TestPowerspectrum(object):
                                        [0.5, 5, 10], [0.3, 10])
 
     def test_intervals_overlapping(self):
-        ref_int = self.vespec._decide_ref_intervals([0.3, 10], [0.5, 6])
+        ref_int = self.vespec._decide_ref_intervals([0.5, 6], [0.3, 10])
         np.testing.assert_allclose(ref_int, [[0.3, 0.5], [6, 10]])
-        ref_int = self.vespec._decide_ref_intervals([0.3, 10], [0.5, 11])
+        ref_int = self.vespec._decide_ref_intervals([0.5, 11], [0.3, 10])
         np.testing.assert_allclose(ref_int, [[0.3, 0.5]])
 
     def test_intervals_non_overlapping(self):
-        ref_int = self.vespec._decide_ref_intervals([0.3, 5], [6, 11])
+        ref_int = self.vespec._decide_ref_intervals([6, 11], [0.3, 5])
         np.testing.assert_allclose(ref_int, [[0.3, 5]])
+
+    def test_construct_lightcurves(self):
+        self.vespec._construct_lightcurves(0.1, [6, 11], [0.3, 5],
+                                           tstart=0, tstop=10)
