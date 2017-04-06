bfdad3b9c0689a6dac371604a3bb8a0d0507511c
Matteo Bachetti <matteo@matteobachetti.it>
2017-04-06 16:02:01 +0200

    Some modifications to VarEnergySpectrum; stub of RmsEnergySpectrum

--- stingray/varenergyspectrum.py
+++ stingray/varenergyspectrum.py
@@ -3,11 +3,13 @@ import numpy as np
 from stingray.gti import check_separate, cross_two_gtis
 from stingray.lightcurve import Lightcurve
 from stingray.utils import assign_value_if_none
+from stingray.crossspectrum import AveragedCrossspectrum
 
 
 class VarEnergySpectrum(object):
     def __init__(self, events, freq_interval, energy_spec, ref_band,
-                 bin_time=1, use_pi=False, log_distr=False):
+                 bin_time=1, use_pi=False, log_distr=False,
+                 segment_size=None):
         """Generic variability-energy spectrum.
         
         Parameters
@@ -43,7 +45,10 @@ class VarEnergySpectrum(object):
 
         self.energy_intervals = list(zip(energies[0: -1], energies[1:]))
         self.ref_band = ref_band
-        self.spectrum = self._spectrum_function()
+
+        self.segment_size = segment_size
+        self.spectrum, self.spectrum_error = self._spectrum_function()
+
 
     def _decide_ref_intervals(self, base_band, ref_band):
         """Eliminate base_band from ref_band."""
@@ -54,8 +59,8 @@ class VarEnergySpectrum(object):
                                                 base_band[1] + 1])]]
         return cross_two_gtis([ref_band], not_base_band)
 
-    def _construct_lightcurves(self, dt, base_band, ref_band,
-                               tstart=None, tstop=None, exclude=True):
+    def _construct_lightcurves(self, base_band, tstart=None, tstop=None,
+                               exclude=True):
         if self.use_pi:
             energies = self.events.pi
         else:
@@ -65,7 +70,8 @@ class VarEnergySpectrum(object):
         tstop = assign_value_if_none(tstop, self.events.time[-1])
 
         good = (energies >= base_band[0]) & (energies < base_band[1])
-        base_lc = Lightcurve.make_lightcurve(self.events.time[good], dt,
+        base_lc = Lightcurve.make_lightcurve(self.events.time[good],
+                                             self.bin_time,
                                              tstart=tstart,
                                              tseg=tstop - tstart,
                                              gti=self.events.gti)
@@ -79,7 +85,8 @@ class VarEnergySpectrum(object):
                             gti=base_lc.gti)
         for i in ref_intervals:
             good = (energies >= i[0]) & (energies < i[1])
-            new_lc = Lightcurve.make_lightcurve(self.events.time[good], dt,
+            new_lc = Lightcurve.make_lightcurve(self.events.time[good],
+                                                self.bin_time,
                                                 tstart=tstart,
                                                 tseg=tstop - tstart,
                                                 gti=self.events.gti)
@@ -92,3 +99,18 @@ class VarEnergySpectrum(object):
 
     def _calculate_stat(self):
         return None
+
+
+class RmsEnergySpectrum(VarEnergySpectrum):
+    def _spectrum_function(self):
+        rms_spec = np.zeros(len(self.energy_intervals))
+        for i, eint in enumerate(self.energy_intervals):
+            base_lc, ref_lc = self._construct_lightcurves(eint)
+            xspect = AveragedCrossspectrum(base_lc, ref_lc,
+                                           segment_size=self.segment_size,
+                                           norm='frac')
+            good = (xspect.freq >= self.freq_interval[0]) & \
+                   (xspect.freq < self.freq_interval[1])
+            rms_spec[i] = np.sqrt(np.sum(xspect.power[good]))
+
+        return rms_spec, None
