5a40c485065d7e7aa5a06faacb41aee455daa5a2
Matteo Bachetti <matteo@matteobachetti.it>
2017-04-06 22:07:38 +0200

    First quasi-full implementation of RmsEnergySpectrum - to be tested

--- stingray/tests/test_varenergyspectrum.py
+++ stingray/tests/test_varenergyspectrum.py
@@ -1,6 +1,6 @@
 import numpy as np
 from stingray.events import EventList
-from stingray.varenergyspectrum import VarEnergySpectrum
+from stingray.varenergyspectrum import VarEnergySpectrum, RmsEnergySpectrum
 
 from astropy.tests.helper import pytest
 
@@ -10,12 +10,16 @@ class TestPowerspectrum(object):
     @classmethod
     def setup_class(cls):
         tstart = 0.0
-        tend = 10.0
+        tend = 100.0
         nphot = 100
-        cls.events = EventList(np.random.uniform(tstart, tend, nphot),
-                               pha=np.random.uniform(0.3, 12, nphot))
+        alltimes = np.random.uniform(tstart, tend, nphot)
+        alltimes.sort()
+        cls.events = EventList(alltimes,
+                               pha=np.random.uniform(0.3, 12, nphot),
+                               gti = [[tstart, tend]])
         cls.vespec = VarEnergySpectrum(cls.events, [0., 10000],
-                                       [0.5, 5, 10], [0.3, 10])
+                                       [0.5, 5, 10], [0.3, 10],
+                                       bin_time=0.1)
         cls.vespeclog = \
             VarEnergySpectrum(cls.events, [0., 10000],
                               [0.5, 5, 10], [0.3, 10], log_distr=True)
@@ -34,9 +38,10 @@ class TestPowerspectrum(object):
         events = EventList([0.09, 0.21, 0.23, 0.32, 0.4, 0.54],
                            pha=[0,0,0,0,1,1])
         vespec = VarEnergySpectrum(events, [0., 10000],
-                                   [0, 1, 2], [0.3, 10])
+                                   [0, 1, 2], [0.5, 1.1],
+                                   bin_time=0.1)
         base_lc, ref_lc = \
-            vespec._construct_lightcurves(0.1, [0, 0.5], [0.5, 1.1],
+            vespec._construct_lightcurves([0, 0.5],
                                           tstart=0, tstop=0.65)
         np.testing.assert_allclose(base_lc.counts, [1, 0, 2, 1, 0, 0])
         np.testing.assert_allclose(ref_lc.counts, [0, 0, 0, 1, 0, 1])
@@ -45,9 +50,10 @@ class TestPowerspectrum(object):
         events = EventList([0.09, 0.21, 0.23, 0.32, 0.4, 0.54],
                            pha=[0,0,0,0,1,1])
         vespec = VarEnergySpectrum(events, [0., 10000],
-                                   [0, 1, 2], [0.3, 10])
+                                   [0, 1, 2], [0, 0.5],
+                                   bin_time=0.1)
         base_lc, ref_lc = \
-            vespec._construct_lightcurves(0.1, [0, 0.5], [0, 0.5],
+            vespec._construct_lightcurves([0, 0.5],
                                           tstart=0, tstop=0.65,
                                           exclude=False)
         np.testing.assert_equal(base_lc.counts, ref_lc.counts)
@@ -56,9 +62,17 @@ class TestPowerspectrum(object):
         events = EventList([0.09, 0.21, 0.23, 0.32, 0.4, 0.54],
                            pi=np.asarray([0, 0, 0, 0, 1, 1]))
         vespec = VarEnergySpectrum(events, [0., 10000],
-                                   [0, 1, 2], [0.3, 10], use_pi=True)
+                                   [0, 1, 2], [0.5, 1.1], use_pi=True,
+                                   bin_time=0.1)
         base_lc, ref_lc = \
-            vespec._construct_lightcurves(0.1, [0, 0.5], [0.5, 1.1],
+            vespec._construct_lightcurves([0, 0.5],
                                           tstart=0, tstop=0.65)
         np.testing.assert_allclose(base_lc.counts, [1, 0, 2, 1, 0, 0])
         np.testing.assert_allclose(ref_lc.counts, [0, 0, 0, 1, 0, 1])
+
+    def test_rmsspectrum_creation(self):
+        rms = RmsEnergySpectrum(self.events, [0., 10000],
+                                [0.3, 12, 2], [5, 10],
+                                bin_time=0.01,
+                                segment_size=30)
+
