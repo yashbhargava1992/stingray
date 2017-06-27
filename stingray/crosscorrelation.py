c26e6d2b2e7711ee67edf5cd4d6d4b9dea0bf8d7
Haroon Rashid <haroon.rashid235@gmail.com>
2017-06-27 23:02:35 +0500

    Refactors code and adds documentation

--- stingray/crosscorrelation.py
+++ stingray/crosscorrelation.py
@@ -9,7 +9,34 @@ from stingray.exceptions import StingrayError
 class CrossCorrelation(object):
     def __init__(self, lc1=None, lc2=None):
 
-        # Create an empty object to populate data later on
+        """
+        Make a cross-correlation from a light curves.
+        You can also make an empty Crosscorrelation object to populate with your
+        own cross-correlation data.
+        Parameters
+        ----------
+        lc1: lightcurve.Lightcurve object, optional, default None
+            The first light curve data for correlation calculations.
+        lc2: lightcurve.Lightcurve object, optional, default None
+            The light curve data for the correlation calculations.
+        
+        Attributes
+        ----------
+         corr: numpy.ndarray
+             An array of correlation data calculated from two lighcurves
+ 
+         time_lags: numpy.ndarray
+             An array of all possible time lags against which each point in corr is calculated 
+ 
+         dt: float
+             The time resolution of each lightcurve (used in time_lag calculations)
+ 
+         time_shift: float
+             Time lag that gives maximum value of correlation between two lightcurves.
+             There will be maximum correlation between lightcurves if one of the lightcurve is shifted by time_shift.    
+        """
+
+        ## Populate all attributes by None if user passes no lightcurve data
         if lc1 is None and lc2 is None:
             self.corr = None
             self.time_shift = None
@@ -17,28 +44,72 @@ class CrossCorrelation(object):
             self.dt = None
             return
         else:
-            if lc1.size != lc2.size:
-                raise StingrayError('Both lightcurves should be of same length')
+            self._make_corr(lc1, lc2)
+
+    def _make_corr(self, lc1, lc2):
+
+        """
+        Creates Crosscorrelation Object.
+        
+        Parameters
+        ----------
+        lc1: lightcurve.Lightcurve object
+            The first light curve data.
+            
+        lc2: lightcurve.Lightcurve object
+            The second light curve data.
+            
+        Returns
+        ----------
+        None
+        """
+
+        # Sizes of both light curves are assumed to be equal for now
+        if lc1.size != lc2.size:
+            raise StingrayError('Both lightcurves should be of same length')
+
+        if not isinstance(lc1, lightcurve.Lightcurve):
+            raise TypeError("lc1 must be a lightcurve.Lightcurve object")
+        if not isinstance(lc2, lightcurve.Lightcurve):
+            raise TypeError("lc2 must be a lightcurve.Lightcurve object")
+
+        if lc1.dt != lc2.dt:
+            raise StingrayError("Light curves do not have "
+                                "same time binning dt.")
+        else:
             self.dt = lc1.dt
-            self.cal_corr(lc1, lc2)
-            self.cal_timeshift()
 
+        # Calculates cross-correlation of two lightcurves
+        self.corr = signal.correlate(lc1.counts, lc2.counts)
 
-def cal_corr(self, lc1, lc2):
-    if not isinstance(lc1, lightcurve.Lightcurve):
-        raise TypeError("lc1 must be a lightcurve.Lightcurve object")
-    if not isinstance(lc2, lightcurve.Lightcurve):
-        raise TypeError("lc2 must be a lightcurve.Lightcurve object")
-    self.corr = signal.correlate(lc1.counts, lc2.counts)
+        self.time_shift, self.time_lags = self.cal_timeshift(dt=self.dt)
 
+    def cal_timeshift(self, dt=1.0):
+        """
+        Creates Crosscorrelation Object.
+        Parameters
+        ----------
+        dt: float , optional, default 1.0
+            Time resolution of lightcurve, should be passed when object is populated with correlation data
+            and no information about light curve can be extracted. Used to calculate time_lags.
+        Returns
+        ----------
+        self.time_shift: float
+             Value of time lag that gives maximum value of correlation between two lightcurves. 
+             
+        self.time_lags: numpy.ndarray
+             An array of time_lags calculated from correlation data 
+        """
 
-def cal_timeshift(self, dt=1):
-    if self.dt is None:
-        self.dt = dt
-
-    n = len(self.corr)
-    dur = int(n / 2)
-    x_lags = np.linspace(-dur, dur, n)
-    self.time_lags = x_lags * self.dt
-    # time_shift is the time of max. correlation
-    self.time_shift = self.time_lags[np.argmax(self.corr)]
+        if self.dt is None:
+            self.dt = dt
+        n = len(self.corr)
+        dur = int(n / 2)
+        # Correlation against all possible lags, positive as well as negative lags are stored
+        x_lags = np.linspace(-dur, dur, n)
+        self.time_lags = x_lags * self.dt
+        # time_shift is the time lag for max. correlation
+        self.time_shift = self.time_lags[np.argmax(self.corr)]
+        
+        return self.time_shift, self.time_lags
+        
