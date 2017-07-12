0f54224b42f5f01dd289d073e41f431182414d92
Haroon Rashid <haroon.rashid235@gmail.com>
2017-07-12 19:14:46 +0500

    Plots for cum3, magnitude and phase bispectrum

--- stingray/bispectrum.py
+++ stingray/bispectrum.py
@@ -216,3 +216,119 @@ class Bispectrum(object):
         self.bispec = fftshift(fft2(ifftshift(self.cum3)))
         self.bispec_mag = np.abs(self.bispec)
         self.bispec_phase = np.angle((self.bispec))
+
+        def plot_cum3(self, axis=None, save=False, filename=False):
+
+        """
+            Plot the 3rd order cumulant as function of time lags using Matplotlib.
+            Plot the self.cum3 on a graph with ``self.lags`` on x-axis and y-axis and
+            ``self.cum3`` on z-axis
+            Parameters
+            ----------
+            axis : list, tuple, string, default None
+                Parameter to set axis properties of Matplotlib figure. For example
+                it can be a list like ``[xmin, xmax, ymin, ymax]`` or any other
+                acceptable argument for `matplotlib.pyplot.axis()` function.
+            save : boolean, optional (default=False)
+                If True, save the figure with specified filename.
+            filename : str
+                File name of the image to save. Depends on the boolean ``save``.
+        """
+
+        try:
+            import matplotlib.pyplot as plt
+        except ImportError:
+            raise ImportError("Matplotlib required for plot()")
+
+        if axis is not None:
+            plt.axis(axis)
+
+        cont = plt.contourf(self.lags, self.lags, self.cum3, 100, cmap=plt.cm.Spectral_r)
+        plt.colorbar(cont)
+        plt.title('3rd Order Cumulant')
+        plt.xlabel('lags 1')
+        plt.ylabel('lags 2')
+        plt.show()
+
+        if save:
+            if filename is None:
+                plt.savefig('bispec_cum3.png')
+            else:
+                plt.savefig(filename)
+
+    def plot_mag(self, axis=None, save=False, filename=False):
+        
+        """
+            Plot the Magnitude of Bispectrum as function of freq using Matplotlib.
+            Plot the self.bispec_mag on a graph with ``self.freq`` on x-axis and y-axis and
+            ``self.bispec_mag`` on z-axis
+            Parameters
+            ----------
+            axis : list, tuple, string, default None
+                Parameter to set axis properties of Matplotlib figure. For example
+                it can be a list like ``[xmin, xmax, ymin, ymax]`` or any other
+                acceptable argument for `matplotlib.pyplot.axis()` function.
+            save : boolean, optional (default=False)
+                If True, save the figure with specified filename.
+            filename : str
+                File name of the image to save. Depends on the boolean ``save``.
+        """
+        try:
+            import matplotlib.pyplot as plt
+        except ImportError:
+            raise ImportError("Matplotlib required for plot()")
+
+        if axis is not None:
+            plt.axis(axis)
+
+        cont = plt.contourf(self.freq, self.freq, self.bispec_mag, 100, cmap=plt.cm.Spectral_r)
+        plt.colorbar(cont)
+        plt.title('Bispectrum Magnitude')
+        plt.xlabel('freq 1')
+        plt.ylabel('freq 2')
+        plt.show()
+
+        if save:
+            if filename is None:
+                plt.savefig('bispec_mag.png')
+            else:
+                plt.savefig(filename)
+
+    def plot_phase(self, axis=None, save=False, filename=False):
+        
+        """
+            Plot the Phase of Bispectrum as function of freq using Matplotlib.
+            Plot the self.bispec_phase on a graph with ``self.phase`` on x-axis and y-axis and
+            ``self.bispec_phase`` on z-axis
+            Parameters
+            ----------
+            axis : list, tuple, string, default None
+                Parameter to set axis properties of Matplotlib figure. For example
+                it can be a list like ``[xmin, xmax, ymin, ymax]`` or any other
+                acceptable argument for `matplotlib.pyplot.axis()` function.
+            save : boolean, optional (default=False)
+                If True, save the figure with specified filename.
+            filename : str
+                File name of the image to save. Depends on the boolean ``save``.
+        """
+        try:
+            import matplotlib.pyplot as plt
+        except ImportError:
+            raise ImportError("Matplotlib required for plot()")
+
+        if axis is not None:
+            plt.axis(axis)
+
+        cont = plt.contourf(self.freq, self.freq, self.bispec_phase, 100, cmap=plt.cm.Spectral_r)
+        plt.colorbar(cont)
+        plt.title('Bispectrum Phase')
+        plt.xlabel('freq 1')
+        plt.ylabel('freq 2')
+        plt.show()
+
+        if save:
+            if filename is None:
+                plt.savefig('bispec_phase.png')
+            else:
+                plt.savefig(filename)
+
