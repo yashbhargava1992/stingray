55ee0f8ec062deac4ef5c8fe9653f74e7c414f15
tappina <mattinam@gmail.com>
2017-07-19 23:20:38 +0200

    Add tests for sinc

---
+++ stingray/pulse/tests/test_modeling.py
@@ -0,0 +1,15 @@
+import numpy as np
+from stingray.pulse.modeling import fitSinc
+
+np.random.seed(0)
+
+def test_sinc_function():
+    x = np.linspace(-5., 5., 200)
+    y = 2 * (np.sin(x)/x)**2
+    y += np.random.normal(0., 0.1, x.shape)
+
+    s = fitSinc(x,y)
+    
+    assert np.abs(s.mean) < 0.1 
+    assert np.abs(s.amplitude - 1) < 0.1 
+    assert np.abs(s.width - 1) < 0.1
