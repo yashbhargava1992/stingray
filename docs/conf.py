571527924b0bbd7a3786de9e9b6b9d70ce7926dd
Daniela Huppenkothen <dhuppenk@uw.edu>
2018-02-01 13:39:04 -0800

    Added new yaml files for notebook integration

--- docs/conf.py
+++ docs/conf.py
@@ -134,7 +134,7 @@ html_theme_options = {
     'logotext3': ':docs'   # white,  light
     }
 
-extensions += ['matplotlib.sphinxext.plot_directive','sphinx.ext.autodoc']
+extensions += ['matplotlib.sphinxext.plot_directive','sphinx.ext.autodoc', 'nbsphinx']
 
 # -- Options for LaTeX output --------------------------------------------------
 
