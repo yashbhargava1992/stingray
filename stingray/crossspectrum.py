Fix of bug in definition of absolute rms normalization of the PSD

stingray/crossspectrum.py
@@ -453,7 +453,7 @@ class Crossspectrum(object):
 
         elif self.norm.lower() == 'abs':
             c = c_num / np.float(self.n ** 2.)
-            power = c * (2. * tseg)
+            power = c * 2. / np.float(tseg)
 
         elif self.norm.lower() == 'none':
             power = unnorm_power