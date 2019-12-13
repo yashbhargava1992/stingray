1b89f1e014cb907593a6f613f8fd552ee87189db
Matteo Bachetti <matteo@matteobachetti.it>
2019-12-13 11:58:01 +0100

    More thorough tests

--- stingray/deadtime/tests/test_fad.py
+++ stingray/deadtime/tests/test_fad.py
@@ -20,7 +20,7 @@ def generate_deadtime_lc(ev, dt, tstart=0, tseg=None, deadtime=2.5e-3):
 
 
 @pytest.mark.parametrize('ctrate', [0.5, 5, 50, 200])
-def test_fad_power_spectrum(ctrate):
+def test_fad_power_spectrum_compliant(ctrate):
     dt = 0.1
     deadtime = 2.5e-3
     length = 25600
@@ -35,18 +35,15 @@ def test_fad_power_spectrum(ctrate):
     ncounts2 = np.sum(lc2.counts)
 
     results = \
-        calculate_FAD_correction(lc1, lc2, segment_size, plot=False,
+        calculate_FAD_correction(lc1, lc2, segment_size, plot=True,
                           smoothing_alg='gauss',
-                          smoothing_length=segment_size*2,
-                          strict=True, verbose=False,
+                          strict=True, verbose=True,
                           tolerance=0.05)
 
     pds1_f = results['pds1']
     pds2_f = results['pds2']
     cs_f = results['cs']
     ptot_f = results['ptot']
-
-    is_compliant = results.meta['is_compliant']
 
     n = length / segment_size
     ncounts_per_intv1 = ncounts1 * segment_size / length
@@ -57,8 +54,34 @@ def test_fad_power_spectrum(ctrate):
     pds_std_theor = 2 / np.sqrt(n)
     cs_std_theor = np.sqrt(2 / n)
 
-    assert is_compliant
     assert np.isclose(pds1_f.std() * 2 / ncounts_per_intv1, pds_std_theor, rtol=0.1)
     assert np.isclose(pds2_f.std() * 2 / ncounts_per_intv2, pds_std_theor, rtol=0.1)
     assert np.isclose(cs_f.std() * 2 / ncounts_per_intv_geomav, cs_std_theor, rtol=0.1)
     assert np.isclose(ptot_f.std() * 2 / ncounts_per_intvtot, pds_std_theor, rtol=0.1)
+
+
+@pytest.mark.parametrize('ctrate', [50])
+def test_fad_power_spectrum_non_compliant(ctrate):
+    dt = 0.1
+    deadtime = 2.5e-3
+    length = 25600
+    segment_size = 256.
+    ncounts = np.int(ctrate * length)
+    ev1 = generate_events(length, ncounts)
+    ev2 = generate_events(length, ncounts)
+
+    lc1 = generate_deadtime_lc(ev1, dt, tstart=0, tseg=length, deadtime=deadtime)
+    lc2 = generate_deadtime_lc(ev2, dt, tstart=0, tseg=length, deadtime=deadtime)
+
+    with pytest.warns(UserWarning) as record:
+        results = \
+            calculate_FAD_correction(lc1, lc2, segment_size, plot=True,
+                              smoothing_alg='gauss',
+                              strict=False, verbose=False,
+                              tolerance=0.0001)
+    assert np.any(["results ARE NOT complying"
+                   in r.message.args[0] for r in record])
+
+    is_compliant = results.meta['is_compliant']
+
+    assert not is_compliant
