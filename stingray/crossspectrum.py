Fixed major bug in PSD and CSD normalization, where meanrate was not defined correctly. Also set tolerances in unit tests to catch this type of issue in the future

stingray/crossspectrum.py
@@ -56,9 +56,7 @@ def _averaged_cospectra_cdf(xcoord, n):
 
     for i, x in enumerate(xcoord):
         prefac_bottom1 = factorial(n - 1)
-        # print("x: " + str(x))
         for j in range(n):
-            # print("j: " + str(j))
             prefac_top = factorial(n - 1 + j)
             prefac_bottom2 = factorial(
                 n - 1 - j) * factorial(j)
@@ -67,15 +65,11 @@ def _averaged_cospectra_cdf(xcoord, n):
             prefac = prefac_top / (
             prefac_bottom1 * prefac_bottom2 * prefac_bottom3)
 
-            # print("prefac: " + str(prefac))
             gf = -j + n
 
-            # print("gamma_fac: " + str(gf))
             first_fac = scipy.special.gamma(gf)
-            # print("first_fac: " + str(first_fac))
             if x >= 0:
                 second_fac = scipy.special.gammaincc(gf, n * x) * first_fac
-                # print("second_fac: " + str(second_fac))
                 fac = 2.0 * first_fac - second_fac
             else:
                 fac = scipy.special.gammaincc(gf, -n * x) * first_fac
@@ -581,7 +575,7 @@ class Crossspectrum(object):
         actual_nphots = np.float64(np.sqrt(np.exp(log_nphots1 + log_nphots2)))
         actual_mean = np.sqrt(self.meancounts1 * self.meancounts2)
 
-        meanrate = np.sqrt((self.nphots1 * self.nphots2) / tseg)
+        meanrate = np.sqrt((self.nphots1 * self.nphots2)) / tseg
 
         assert actual_mean > 0.0, \
             "Mean count rate is <= 0. Something went wrong."

stingray/tests/test_crossspectrum.py
@@ -323,7 +323,7 @@ class TestCrossspectrum(object):
         assert cs.norm == 'abs'
         abs_noise = 2. * self.rate1  # expected Poisson noise level
         print(np.mean(cs.power), abs_noise)
-        assert np.isclose(np.mean(cs.power[1:]), abs_noise, atol=30)
+        assert np.isclose(np.mean(cs.power[1:]), abs_noise)
 
     def test_norm_leahy(self):
         with pytest.warns(UserWarning) as record:
@@ -332,13 +332,15 @@ class TestCrossspectrum(object):
         assert cs.norm == 'leahy'
         leahy_noise = 2.0  # expected Poisson noise level
         print(np.mean(cs.power), leahy_noise)
-        assert np.isclose(np.mean(cs.power[1:]), leahy_noise, atol=0.2)
+        assert np.isclose(np.mean(cs.power[1:]), leahy_noise, rtol=0.02)
 
     def test_norm_frac(self):
         with pytest.warns(UserWarning) as record:
-            cs = Crossspectrum(self.lc1, self.lc2, norm='frac')
+            cs = Crossspectrum(self.lc1, self.lc1, norm='frac')
         assert len(cs.power) == 4999
         assert cs.norm == 'frac'
+        norm = 2. / self.rate1
+        assert np.isclose(np.mean(cs.power[1:]), norm, rtol=0.1)
 
     def test_norm_abs(self):
         with pytest.warns(UserWarning) as record: