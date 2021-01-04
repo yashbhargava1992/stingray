2a2d779f8ab630becef25d487af4d0f99957f057
Matteo Bachetti <matteo@matteobachetti.it>
2021-01-04 16:49:10 +0100

    Fix test failures due to (I think) scipy using lists

--- stingray/utils.py
+++ stingray/utils.py
@@ -101,6 +101,7 @@ __all__ = ['simon', 'rebin_data', 'rebin_data_log', 'look_for_array_in_array',
 
 
 def _root_squared_mean(array):
+    array = np.asarray(array)
     return np.sqrt(np.sum(array ** 2)) / len(array)
 
 
