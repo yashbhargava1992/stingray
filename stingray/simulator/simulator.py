Fixed normalization again

stingray/simulator/simulator.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 import numbers
+import warnings
 from scipy import signal
 import astropy.modeling.models
 from stingray.io import write, read
@@ -34,16 +35,25 @@ class Simulator(object):
         seed value for random processes
     """
 
-    def __init__(self, dt=1, N=1024, mean=0, rms=1, red_noise=1,
+    def __init__(self, dt, N, mean, rms, red_noise=1,
                  random_state=None, tstart=0.0):
         self.dt = dt
+ 
+        if not isinstance(N, (int, np.integer)):
+            raise ValueError("N must be integer!") 
+ 
         self.N = N
+
+        if mean == 0:
+            warnings.warn("Careful! A mean of zero is unphysical!" + \
+                          "This may have unintended consequences!")
         self.mean = mean
         self.nphot = self.mean * self.N
         self.rms = rms
         self.red_noise = red_noise
         self.tstart = tstart
         self.time = dt*np.arange(N) + self.tstart
+        self.freq = np.fft.rfftfreq(self.N, d=self.dt)[1:]
 
         # Initialize a tuple of energy ranges with corresponding light curves
         self.channels = []
@@ -340,23 +350,16 @@ class Simulator(object):
         a2 = self.random_state.normal(size=len(w))
 
         psd = np.power((1/w),B)
+
         if self.nphot == 0:
             nphot = 1.0
         else:
             nphot = self.nphot
         self.std = np.sqrt((nphot / (self.N**2.)) * (np.sum(psd[:-1]) + 0.5*psd[-1]))
 
-#        print("first term: " + str((self.nphot / (self.N**2.))))
-#        print("sum: " + str((np.sum(psd[:-1]) + 0.5*psd[-1])))  
-#        print("Whole term: " + str(np.sqrt((self.nphot / (self.N**2.)) * (np.sum(psd[:-1]) + 0.5*psd[-1]))))
-  
-#        print("nphot: " + str(self.nphot))
-#        print("N: " + str(self.N)) 
-
-
         # Multiply by (1/w)^B to get real and imaginary parts
-        real = a1 * np.sqrt(psd)
-        imaginary = a2 * np.sqrt(psd)
+        real = a1 * np.sqrt(psd*self.nphot/4)
+        imaginary = a2 * np.sqrt(psd*self.nphot/4)
 
         # Obtain time series
         long_lc = self._find_inverse(real, imaginary)
@@ -382,7 +385,12 @@ class Simulator(object):
         # Cast spectrum as numpy array
         s = np.array(s)
 
-        self.std = np.sqrt((self.nphot / (self.N**2.)) * (np.sum(s[:-1]) + 0.5*s[-1]))
+
+        if self.nphot == 0:
+            nphot = 1.0
+        else:
+            nphot = self.nphot
+        self.std = np.sqrt((nphot / (self.N**2.)) * (np.sum(s[:-1]) + 0.5*s[-1]))
 
         self.red_noise = 1
 
@@ -390,8 +398,8 @@ class Simulator(object):
         a1 = self.random_state.normal(size=len(s))
         a2 = self.random_state.normal(size=len(s))
 
-        real = a1 * np.sqrt(s)
-        imaginary = a2 * np.sqrt(s)
+        real = a1 * self.nphot * np.sqrt(s) / 4.0
+        imaginary = a2 * self.nphot * np.sqrt(s) / 4.0
 
         lc = self._find_inverse(real, imaginary)
         lc = Lightcurve(self.time, self._extract_and_scale(lc),
@@ -424,9 +432,13 @@ class Simulator(object):
         # Compute PSD from model
         simpsd = model(simfreq)
 
-        self.std = np.sqrt((self.nphot / (self.N**2.)) * (np.sum(simpsd[:-1]) + 0.5*simpsd[-1]))
+        if self.nphot == 0:
+            nphot = 1.0
+        else:
+            nphot = self.nphot
+        self.std = np.sqrt((nphot / (self.N**2.)) * (np.sum(simpsd[:-1]) + 0.5*simpsd[-1]))
 
-        fac = np.sqrt(simpsd)
+        fac = np.sqrt(simpsd) * self.nphot / 4.0
         pos_real   = self.random_state.normal(size=nbins//2)*fac
         pos_imag   = self.random_state.normal(size=nbins//2)*fac
 
@@ -469,9 +481,13 @@ class Simulator(object):
             else:
                 raise ValueError('Params should be list or dictionary!')
 
-            self.std = np.sqrt((self.nphot / (self.N**2.)) * (np.sum(simpsd[:-1]) + 0.5*simpsd[-1]))
+            if self.nphot == 0:
+                nphot = 1.0
+            else:
+                nphot = self.nphot
+            self.std = np.sqrt((nphot / (self.N**2.)) * (np.sum(simpsd[:-1]) + 0.5*simpsd[-1]))
 
-            fac = np.sqrt(simpsd)
+            fac = np.sqrt(simpsd) * self.nphot / 4.0
             pos_real   = self.random_state.normal(size=nbins//2)*fac
             pos_imag   = self.random_state.normal(size=nbins//2)*fac
 
@@ -580,12 +596,12 @@ class Simulator(object):
                                           self.red_noise*self.N - self.N+1)
             lc = np.take(long_lc, range(extract, extract + self.N))
 
-        avg = np.mean(lc)
-        std = np.std(lc)
-        print(f"self.mean: {self.mean}")
-        print(f"self.std: {self.std}")
+        mean_lc = np.mean(lc)
 
-        return (lc-avg)/std * self.mean * self.rms + self.mean
+        if self.mean == 0:
+            return (lc-mean_lc)/self.std * 0.5*self.rms
+        else:
+            return (lc-mean_lc)/self.std * self.mean * self.rms + self.mean
 
     def powerspectrum(self, lc, seg_size=None):
         """