e0675a02f0ea4b49dbed6afabbb8c4c690792e95
Matteo Bachetti <matteo@matteobachetti.it>
2022-01-18 08:21:21 +0100

    Eliminate test of eliminated method

--- stingray/tests/test_varenergyspectrum.py
+++ stingray/tests/test_varenergyspectrum.py
@@ -65,16 +65,6 @@ class TestVarEnergySpectrum(object):
         with pytest.raises(TypeError):
             ref_int = VarEnergySpectrum(self.events, [0.0, 10000], (0.5, 5, 10, "log"), [0.3, 10])
 
-    def test_intervals_overlapping(self):
-        ref_int = self.vespec._decide_ref_intervals([0.5, 6])
-        np.testing.assert_allclose(ref_int, [[0.3, 0.5], [6, 10]])
-        ref_int = self.vespec._decide_ref_intervals([0.5, 11])
-        np.testing.assert_allclose(ref_int, [[0.3, 0.5]])
-
-    def test_intervals_non_overlapping(self):
-        ref_int = self.vespec._decide_ref_intervals([10.5, 11])
-        np.testing.assert_allclose(ref_int, [[0.3, 10]])
-
     def test_ref_band_none(self):
         events = EventList(
             [0.09, 0.21, 0.23, 0.32, 0.4, 0.54], energy=[0, 0, 0, 0, 1, 1], gti=[[0, 0.65]]
