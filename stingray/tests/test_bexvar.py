bc6cf564a4103b4a87309dd49da148a85cdd7d8e
Mihir Tripathi <mihirtripathi97@gmail.com>
2022-08-05 12:32:36 +0530

    Additional tests

--- stingray/tests/test_bexvar.py
+++ stingray/tests/test_bexvar.py
@@ -5,6 +5,8 @@ import numpy as np
 import scipy.stats
 from stingray import bexvar
 from astropy.table import Table
+import operator
+from astropy.io import fits
 
 
 _HAS_ULTRANEST = True
@@ -33,19 +35,54 @@ class TestBexvarResult(object):
         cls.frac_exp = lightcurve["FRACEXP"][:,band]
 
         cls.fname_result = os.path.join(datadir,"bexvar_results_band_0.npy")
+        cls.quantile = scipy.stats.norm().cdf([-1])       
 
     @pytest.mark.skipif("not _HAS_ULTRANEST")
     def test_bexvar(self):
         log_cr_sigma_from_function = bexvar.bexvar(self.time, self.time_delta, self.src_counts, self.bg_counts, self.bg_ratio, self.frac_exp)
         log_cr_sigma_result = np.load(self.fname_result,allow_pickle=True)[1]
 
-        quantile = scipy.stats.norm().cdf([-1])
-        scatt_lo_function = scipy.stats.mstats.mquantiles(log_cr_sigma_from_function, quantile)
-        scatt_lo_result = scipy.stats.mstats.mquantiles(log_cr_sigma_result, quantile)
+        scatt_lo_function = scipy.stats.mstats.mquantiles(log_cr_sigma_from_function, cls.quantile)
+        scatt_lo_result = scipy.stats.mstats.mquantiles(log_cr_sigma_result, cls.quantile)
         
         # Compares lower 1 sigma quantile of the estimated scatter of the log(count rate) in dex 
         assert np.isclose(scatt_lo_function, scatt_lo_result, rtol = 0.1)
 
+    @pytest.mark.skipif("not _HAS_ULTRANEST")
+    def test_if_bg_counts_none(self):
+        log_cr_sigma = bexvar.bexvar(time = self.time, time_del = self.time_delta, \
+                                        src_counts = self.src_counts, bg_counts = None, \
+                                        bg_ratio = self.bg_ratio, frac_exp = self.frac_exp)
+      
+        scatt_lo = scipy.stats.mstats.mquantiles(log_cr_sigma, self.quantile)
+        assert np.isclose(scatt_lo, 0.0143, rtol = 0.1)
+
+    @pytest.mark.skipif("not _HAS_ULTRANEST")
+    def test_if_bg_ratio_none(self):
+        log_cr_sigma = bexvar.bexvar(time = self.time, time_del = self.time_delta, \
+                                        src_counts = self.src_counts, bg_counts = self.bg_counts, \
+                                        bg_ratio = None, frac_exp = self.frac_exp)
+      
+        scatt_lo = scipy.stats.mstats.mquantiles(log_cr_sigma, self.quantile)
+        assert np.isclose(scatt_lo, 0.0106, rtol = 0.1)
+
+    @pytest.mark.skipif("not _HAS_ULTRANEST")
+    def test_if_frac_exp_none(self):
+        log_cr_sigma = bexvar.bexvar(time = self.time, time_del = self.time_delta, \
+                                        src_counts = self.src_counts, bg_counts = self.bg_counts, \
+                                        bg_ratio = self.bg_ratio, frac_exp = None)
+        
+        scatt_lo = scipy.stats.mstats.mquantiles(log_cr_sigma, self.quantile)
+        assert np.isclose(scatt_lo, 0.0100, rtol = 0.1)
+
+    @pytest.mark.skipif("not _HAS_ULTRANEST")
+    def test_if_all_optional_param_none(self):
+        log_cr_sigma = bexvar.bexvar(time = self.time, time_del = self.time_delta, \
+                                        src_counts = self.src_counts, bg_counts = None, \
+                                        bg_ratio = None, frac_exp = None)
+        
+        scatt_lo = scipy.stats.mstats.mquantiles(log_cr_sigma, self.quantile)
+        assert np.isclose(scatt_lo, 0.0100, rtol = 0.1)
 
 class TestInternalFunctions(object):
     @classmethod
