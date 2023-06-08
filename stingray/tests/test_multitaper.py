9347304ccd0b747ee36ba9b3d29acdfa28a65d4d
Matteo Bachetti <matteo@matteobachetti.it>
2023-06-08 22:30:54 +0200

    Fix calls in test_multitaper

--- stingray/tests/test_multitaper.py
+++ stingray/tests/test_multitaper.py
@@ -149,12 +149,12 @@ class TestMultitaper(object):
         lc = Lightcurve(time, counts=poisson_counts, dt=1, gti=[[0, 100]])
         mtp = Multitaper(lc, norm="leahy")
         rms_mtp_l, rms_err_l = mtp.compute_rms(
-            min_freq=mtp.freq[1], max_freq=mtp.freq[-1], white_noise_offset=0
+            min_freq=mtp.freq[1], max_freq=mtp.freq[-1], poisson_noise_level=0
         )
 
         mtp = Multitaper(lc, norm="frac")
         rms_mtp, rms_err = mtp.compute_rms(
-            min_freq=mtp.freq[1], max_freq=mtp.freq[-1], white_noise_offset=0
+            min_freq=mtp.freq[1], max_freq=mtp.freq[-1], poisson_noise_level=0
         )
         assert np.allclose(rms_mtp, rms_mtp_l, atol=0.01)
         assert np.allclose(rms_err, rms_err_l, atol=0.01)
