53521ff3f7ba9cdcf2dcf0992cc3b23182fc180d
pupperemeritus <pupperemeritus9@protonmail.com>
2023-09-22 19:50:31 +0530

    Removed largememory

--- stingray/__init__.py
+++ stingray/__init__.py
@@ -22,5 +22,4 @@ if not _ASTROPY_SETUP_:
     from stingray.stats import *
     from stingray.bispectrum import *
     from stingray.varenergyspectrum import *
-    from stingray.largememory import *
     from stingray.lombscargle import *
