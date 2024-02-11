ac688e4bd22bb8cd0f685711e0a96fbe74dc2b73
Abhinav Kumar <kumar.kr.abhinav@gmail.com>
2024-02-11 13:01:38 +0530

    Eliminated the usage of astropy logging

---
+++ stingray/loggingconfig.py
@@ -0,0 +1,33 @@
+import logging
+
+class CustomFormatter(logging.Formatter):
+
+    grey = "\x1b[38;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: grey + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+    
+
+def setup_logger():
+    logger = logging.getLogger(__name__)
+    handler = logging.StreamHandler()
+    formatter = CustomFormatter()
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
+    return logger
