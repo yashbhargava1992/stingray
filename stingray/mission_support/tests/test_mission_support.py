6702affe62eab6586a0ba46da34376a58b7545b9
Matteo Bachetti <matteo@matteobachetti.it>
2024-05-07 09:43:26 +0200

    Add test for wrong mission

--- stingray/mission_support/tests/test_mission_support.py
+++ stingray/mission_support/tests/test_mission_support.py
@@ -1,10 +1,20 @@
 import pytest
 import os
 from astropy.io import fits
-from stingray.mission_support import rxte_pca_event_file_interpretation
+from stingray.mission_support import (
+    rxte_pca_event_file_interpretation,
+    get_rough_conversion_function,
+)
 
 curdir = os.path.abspath(os.path.dirname(__file__))
 datadir = os.path.join(curdir, "..", "..", "tests", "data")
+
+
+class TestAll(object):
+
+    def test_wrong_mission_raises(self):
+        with pytest.raises(ValueError, match="Mission blah not recognized"):
+            get_rough_conversion_function("blah")
 
 
 class TestXTE(object):
