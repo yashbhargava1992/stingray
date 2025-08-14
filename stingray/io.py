8b1016c5ab9bc402d423e12e16b712dd3da16feb
Matteo Bachetti <matteo@matteobachetti.it>
2025-08-14 21:54:24 +0200

    Use the timepixr keyword when calculating times Timepixr is 0.5 by default
    Simplify logic

--- stingray/io.py
+++ stingray/io.py
@@ -619,9 +619,17 @@ def load_events_and_gtis(
 
     det_number = None if detector_id is None else list(set(detector_id))
 
+    timedel = np.longdouble(0.0)
+    if "TIMEDEL" in header:
+        timedel = np.longdouble(header["TIMEDEL"])
     timezero = np.longdouble(0.0)
     if "TIMEZERO" in header:
         timezero = np.longdouble(header["TIMEZERO"])
+
+    if "TIMEPIXR" in header:
+        timepixr = np.longdouble(header["TIMEPIXR"])
+
+        timezero += (0.5 - timepixr) * timedel
 
     ev_list += timezero
 
@@ -963,8 +971,12 @@ class FITSTimeseriesReader(object):
         header = hdulist[hduname].header
         if "OBS_ID" in header:
             self._add_meta_attr("obsid", header["OBS_ID"])
+
+        timedel = np.longdouble(0.0)
         if "TIMEDEL" in header:
-            self._add_meta_attr("dt", header["TIMEDEL"])
+            timedel = np.longdouble(header["TIMEDEL"])
+
+        self._add_meta_attr("dt", timedel)
 
         # self.header has to be a string, for backwards compatibility and... for convenience!
         # No need to cope with dicts working badly with Netcdf, for example. The header
@@ -990,12 +1002,19 @@ class FITSTimeseriesReader(object):
         timezero = np.longdouble(0.0)
         if "TIMEZERO" in header:
             timezero = np.longdouble(header["TIMEZERO"])
+
+        if "TIMEPIXR" in header:
+            timepixr = np.longdouble(header["TIMEPIXR"])
+
+            timezero += (0.5 - timepixr) * timedel
+
         t_start = t_stop = None
         if "TSTART" in header:
             t_start = np.longdouble(header["TSTART"])
         if "TSTOP" in header:
             t_stop = np.longdouble(header["TSTOP"])
         self._add_meta_attr("timezero", timezero)
+
         self._add_meta_attr("t_start", t_start)
         self._add_meta_attr("t_stop", t_stop)
 
